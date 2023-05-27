9707c88805427dd2b66cebc2cc6d23d48cd3179b
maze02 <mariasb2@gmail.com>
2023-05-27 18:58:55 +0200

    Main Layout Folders and Styling added

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,29 @@
 //The main entry point of our application
 <template>
+    <the-header></the-header>
     <router-view></router-view>
 </template>
+<script>
+import TheHeader from "./components/layout/TheHeader.vue";
+
+export default { 
+    components: { 
+        TheHeader // importing it because it is a local component
+    }
+};
+</script>
+<style>
+    @import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap");
+
+    * {
+    box-sizing: border-box;
+    }
+
+    html {
+    font-family: "Roboto", sans-serif;
+    }
+
+    body {
+    margin: 0;
+    }
+</style>
